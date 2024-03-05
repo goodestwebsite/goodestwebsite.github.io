fa08b8f8932c6ce3ac94ad8ead230b2fd138e670
Ethan <meareveryverygood@gmail.com>
2024-03-05 19:59:19 +0800

    Initial commit

--- filetest/read.js
+++ filetest/read.js
@@ -1,7 +1,8 @@
 function read(){
+    // console.log("oklol") ;
     var fs = require('fs')
-    document.getElementById("x").innerHTML="ok" ;
-    fs.writeFile('test.txt', 'Append tex', function (err) {
+    // document.getElementById("x").innerHTML="ok" ;
+    fs.appendFile('test.txt', 'f aefesfcf tex', function (err) {
         if (err) throw err;
         console.log('Updated!');
     });
