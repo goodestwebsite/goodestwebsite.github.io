75f61a478bed6121fa64a8135feb70e1205c5c87
Ethan <meareveryverygood@gmail.com>
2024-03-05 20:03:42 +0800

    Goodestwebsite

--- filetest/read.js
+++
@@ -1,9 +0,0 @@
-function read(){
-    // console.log("oklol") ;
-    var fs = require('fs')
-    // document.getElementById("x").innerHTML="ok" ;
-    fs.appendFile('test.txt', 'f aefesfcf tex', function (err) {
-        if (err) throw err;
-        console.log('Updated!');
-    });
-}
--- login.js
+++
@@ -1,7 +0,0 @@
-<script src="/cookie.js"></script>
-
-function logout(){
-    deletecookie() ;
-    if (getCookie("username") == -1) document.getElementById("sign").innerHTML = "You haven't signed in!" ;
-    else document.getElementById("sign").innerHTML = "Sucessfully logged out." ;
-}
