2aa8652c136771065a01fd0c4740e795e323fbc8
Ethan <meareveryverygood@gmail.com>
2024-04-04 18:47:55 +0800

    goodest

---
+++ getlink.js
@@ -0,0 +1,10 @@
+function getlink(){
+    document.getElementById("link1").href = "/tools" ;
+    document.getElementById("link1").innerHTML = "Tools" ;
+    document.getElementById("link2").href = "/bruh/rickroll" ;
+    document.getElementById("link2").innerHTML = "Free money" ;
+    document.getElementById("link3").href = "/list" ;
+    document.getElementById("link3").innerHTML = "Others" ;
+    // document.getElementById("4").href = "idk" ;
+    // document.getElementById("4").innerHTML = "idk" ;
+}
